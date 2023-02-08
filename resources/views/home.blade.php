4a6a13fb491c7cffd70c65130c632616482e9b91
MyUsername <my@email.com>
2023-02-08 16:06:06 +0100

    insert img and logo navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,13 @@
 @extends('layout.app')
+
+@section('main-content')
+
+   <div class="container">
+      <div class="row">
+         <div class="col-12">
+            <h1>bla</h1>
+         </div>
+      </div>
+   </div>
+
+@endsection
