65d6d19b5831a9a1b0e11ce739c4d8d8531d69c2
MyUsername <my@email.com>
2023-02-08 16:43:38 +0100

    print on dom all card with db info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,20 @@
 @section('main-content')
 
 <div class="comics">
-   <div class="container bg-dark p-5">
+   <div class="container-fluid bg-dark p-5">
       <div class="row">
+         @foreach ($comics as $comics)
          <div class="col-4">
             <div class="card">
-
+               <img src="{{ $comics['thumb'] }}" alt="">
+               <div class="description-card">
+                  <h1>{{ $comics['title'] }}</h1>
+                  <h2>{{ $comics['description'] }}</h2>
+                  <h4>{{ $comics['price'] }}</h4>
+               </div>
             </div>
          </div>
+         @endforeach
       </div>
    </div>
 </div>
