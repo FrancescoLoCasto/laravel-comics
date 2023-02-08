f6c781da4804c330b23a0cb7040082b4a7b8755e
MyUsername <my@email.com>
2023-02-08 17:14:37 +0100

    insert jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,29 @@
 
 @section('main-content')
 
-<div class="comics">
-   <div class="container-fluid bg-dark p-5">
-      <div class="row">
-         @foreach ($comics as $comics)
-         <div class="col-4">
-            <div class="card">
-               <img src="{{ $comics['thumb'] }}" alt="">
-               <div class="description-card">
-                  <h1>{{ $comics['title'] }}</h1>
-                  <h2>{{ $comics['description'] }}</h2>
-                  <h4>{{ $comics['price'] }}</h4>
+   <div class="container-fluid my-banner">
+      <img src="{{ Vite::asset('resources/assets/img/jumbotron.jpg') }}" alt="">
+   </div>
+
+   <div class="comics">
+      <div class="container-fluid bg-dark p-5">
+         <div class="row">
+            @foreach ($comics as $comics)
+            <div class="col-4">
+               <div class="card">
+                  <img src="{{ $comics['thumb'] }}" alt="">
+                  <div class="description-card">
+                     <h1>{{ $comics['title'] }}</h1>
+                     <h2>{{ $comics['description'] }}</h2>
+                     <h4>Serie: {{ $comics['series'] }}</h4>
+                     <h4>Data di uscita: {{ $comics['sale_date'] }}</h4>
+                     <h4>Prezzo: {{ $comics['price'] }}</h4>
+                  </div>
                </div>
             </div>
+            @endforeach
          </div>
-         @endforeach
       </div>
    </div>
-</div>
 
 @endsection
